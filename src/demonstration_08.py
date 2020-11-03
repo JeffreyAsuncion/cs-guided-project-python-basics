a27c0c7570dc3f4c14279ae9d5818bf6492a8f4a
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-03 13:26:46 -0500

    demp#08.py pseudo code

--- src/demonstration_08.py
+++ src/demonstration_08.py
@@ -10,6 +10,17 @@ Examples:
 - num_args(True, False) ➞ 2
 - num_args({}) ➞ 1
 """
-def num_args():
+def num_args(*args):
+
     # Your code here
+    # look up how to find the number of arguments in a python function
+    # using len() method in args to count
+    return(len(args))
+
+print(num_args()) # ➞ 0
+print(num_args("foo")) # ➞ 1
+print(num_args("foo", "bar")) # ➞ 2
+print(num_args(True, False)) # ➞ 2
+print(num_args({})) # ➞ 1
+
 
