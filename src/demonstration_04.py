325ac707b69a4c06be97ff4aadc7a62601762793
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-03 12:54:15 -0500

    demp#04.py pseudo code

--- src/demonstration_04.py
+++ src/demonstration_04.py
@@ -9,9 +9,14 @@ Examples:
 - find_perimeter(20, 10) ➞ 60
 - find_perimeter(2, 9) ➞ 22
 """
+
 def find_perimeter(length, width):
+
     # Your code here
-    return 2*length + 2* width
+    # to get the perimeter of a rectangle we could use either (l * 2 + w * 2) or (l + w) * 2
+    # return the value of the expression 2* length + 2 * width
+    return 2 * length + 2 * width
+
 
 print(find_perimeter(6, 7)) # 26
 print(find_perimeter(20, 10)) # 60
